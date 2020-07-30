07110b494190c3e2cd1615a4489abae2a916ff56
Bo Niu <niubo19930402@hotmail.com>
2020-07-30 06:32:18 -0700

    update db.js to add filter

--- api/db.js
+++ api/db.js
@@ -4,24 +4,58 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.DB_URL || 'mongodb://localhost/SeattleCrime';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
   db = client.db();
 }
 
-async function getNextSequence(name) {
-  const result = await db.collection('counters').findOneAndUpdate(
-    { _id: name },
-    { $inc: { current: 1 } },
-    { returnOriginal: false },
-  );
-  return result.value.current;
-}
+// async function getNextSequence(name) {
+//   const result = await db.collection('crimes').findOneAndUpdate(
+//     { _id: name },
+//     { $inc: { current: 1 } },
+//     { returnOriginal: false },
+//   );
+//   return result.value.current;
+// }
 
 function getDb() {
   return db;
 }
 
-module.exports = { connectToDb, getNextSequence, getDb };
+async function filtrateCrimes({
+  startDate, endDate, district, beat,
+}) {
+  const andList = [];
+  if (startDate) {
+    andList.push({
+      OffenseStartDate: {
+        $gte: startDate.toISOString(),
+      },
+    });
+  }
+  if (endDate) {
+    andList.push({
+      OffenseStartDate: {
+        $lt: endDate.toISOString(),
+      },
+    });
+  }
+  if (district) {
+    andList.push({
+      District: district,
+    });
+  }
+  if (beat) {
+    andList.push({
+      Beat: beat,
+    });
+  }
+  const result = db.collection('crimes').find({
+    $and: andList,
+  });
+  return result.value;
+}
+
+module.exports = { connectToDb, getDb, filtrateCrimes };
